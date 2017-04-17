11643de28ec5dd5e2623ed7ec308776e10d6a285
John Goodwyn <jgoodwyn4@gmail.com>
2017-04-17 14:45:34 -0400

    Added the VoteCounter class

---
+++ VoteCounter.java
@@ -0,0 +1,27 @@
+
+
+public class VoteCounter
+{
+    private int choiceID;
+    private int raceID;
+    private int ballotID;
+    private int electionID;
+
+    private int count;
+
+    public VoteCounter(int cID, int rID, int bID, int eID)
+    {
+	choiceID = cID:
+	raceID = rID;
+	ballotID = bID;
+	electionID = eID;
+
+	count = 0;
+    }
+
+    public void up() { count++; }
+
+    public void down() { count--; }
+
+    public int getCount() { return count; }
+}
