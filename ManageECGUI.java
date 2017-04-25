a7ef5214a82f36d5ffa1b7ff49ef6e30165011f8
John Goodwyn <jgoodwyn4@gmail.com>
2017-04-24 21:50:50 -0400

    More Layout updates to the ManageEC GUI

--- ManageECGUI.java
+++ ManageECGUI.java
@@ -4,11 +4,10 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.ArrayList;
 
-class ManageECGUI implements ActionListener
+class ManageECGUI extends JFrame implements ActionListener
 {
     // Attributes
     private ArrayList<JPanel> ecPanel;
-    private JFrame window;
     private JPanel ecContainer;
     private ECList holder;
 
@@ -18,12 +17,14 @@ class ManageECGUI implements ActionListener
 	ArrayList<String> nameList = holder.getList();
 	ecPanel = new ArrayList<JPanel>();
 	
-        window = new JFrame("Manage Election Commissioners");
+	setTitle("Manage Election Commissioners");
 	JPanel display = new JPanel();
 
 	BoxLayout layout = new BoxLayout(display,BoxLayout.PAGE_AXIS);
-	//GridLayout layout = new GridLayout(0,1);
 	display.setLayout(layout);
+
+	// Gap used to space out each panel
+        display.add(Box.createRigidArea(new Dimension(0,10)));
 
 	JLabel info = new JLabel("Add and/or Remove Election Commissioners:");
 	info.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -66,16 +67,16 @@ class ManageECGUI implements ActionListener
 	// Gap after close button
 	display.add(Box.createRigidArea(new Dimension(0,10)));
 
-	window.setSize(400,350);
-	window.setResizable(false);
+        setSize(400,360);
+        setResizable(false);
 	
-        window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        window.getContentPane().add(display);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        getContentPane().add(display);
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - window.getWidth()) /2 );
-        int y = (int) ((dimension.getHeight() - window.getHeight()) /2);
-        window.setLocation(x, y);
-        window.setVisible(true);
+        int x = (int) ((dimension.getWidth() - getWidth()) /2 );
+        int y = (int) ((dimension.getHeight() - getHeight()) /2);
+        setLocation(x, y);
+        setVisible(true);
 	
     }
 
@@ -90,18 +91,25 @@ class ManageECGUI implements ActionListener
 		String name = nameList.get(i);
 		
 		JPanel temp = new JPanel();
-		temp.setLayout(new FlowLayout());
-		//temp.setLayout(new GridLayout(1,2,0,0));
+		temp.setLayout(new BoxLayout(temp,BoxLayout.LINE_AXIS));
 
+		
+		// Gap before name
+		temp.add(Box.createRigidArea(new Dimension(90,0)));
+		
 		JLabel namePanel = new JLabel(name);
 		temp.add(namePanel);
+
+		// Gap between name and remove button
+		temp.add(Box.createHorizontalGlue());
 		
 		JButton removeButton = new JButton("X");
 		removeButton.addActionListener(this);
 		removeButton.setActionCommand(name);
 		temp.add(removeButton);
 
-		//temp.setBorder(new EmptyBorder(10,10,10,10));
+		// Gap after remove button
+		temp.add(Box.createRigidArea(new Dimension(90,0)));
 
 		ecPanel.add(temp);
 	    }
@@ -122,29 +130,29 @@ class ManageECGUI implements ActionListener
     private void addComponents()
     {
 	for(JPanel ec : ecPanel)
-	    ecContainer.add(ec);
+	    {
+		// Gap used to space out each panel
+	        ecContainer.add(Box.createRigidArea(new Dimension(0,5)));
+		
+		ecContainer.add(ec);
+
+		// Gap used to space out each panel
+	        ecContainer.add(Box.createRigidArea(new Dimension(0,5)));
+	    }
     }
 
     public void actionPerformed(ActionEvent e)
     {
 	if(e.getActionCommand().equals("closeEC"))
 	    {
-		window.dispose();
+	        this.dispose();
 	    }
 	else if(e.getActionCommand().equals("addEC"))
 	    {
-		// Open user input dialog box
-
-		
-	        new ecDialog();
-		//updateList();
+		new ecDialog();
 	    }
 	else
 	    {
-		//String insert = e.getActionCommand();
-		
-		//JOptionPane.showMessageDialog(null,"Remove Command for: " + insert,"Test",JOptionPane.PLAIN_MESSAGE);
-
 		holder.removeEC(e.getActionCommand());
 	        updateList();
 	    }
@@ -166,17 +174,51 @@ class ManageECGUI implements ActionListener
 	    
 	    JPanel dialogPanel = new JPanel();
 	    
-	    //BoxLayout layout = new BoxLayout(dialogPanel,BoxLayout.Y_AXIS);
-	    dialogPanel.setLayout(new BoxLayout(dialogPanel,BoxLayout.Y_AXIS));
+	    dialogPanel.setLayout(new BoxLayout(dialogPanel,BoxLayout.PAGE_AXIS));
+
+	    // Gap between the top of the window and the first JLabel
+	    dialogPanel.add(Box.createRigidArea(new Dimension(0,10)));
 	    
-	    JLabel instruct = new JLabel("Enter the Election Commissioner Username to be added:");
+	    JLabel instruct = new JLabel("Enter a Username to be added:");
+	    instruct.setAlignmentX(Component.CENTER_ALIGNMENT);
 	    dialogPanel.add(instruct);
+
+	    // Gap between JLabel and JPanel
+	    dialogPanel.add(Box.createRigidArea(new Dimension(0,10)));
+
+	    /* ********************************************************** */
+	    /*  Create a horizontal group to contain the Username: input  */
+	    /* ********************************************************** */
+	    
+	    JPanel horizGroup = new JPanel();
+	    horizGroup.setLayout(new BoxLayout(horizGroup,BoxLayout.LINE_AXIS));
+
+	    // Create glue/gap between username prompt
+	    //horizGroup.add(Box.createHorizontalGlue());
+	    horizGroup.add(Box.createRigidArea(new Dimension(50,0)));
 	    
 	    JLabel userLabel = new JLabel("Username: ");
-	    dialogPanel.add(userLabel);
+	    horizGroup.add(userLabel);
+
+	    // Gap between Username label and text field
+	    //horizGroup.add(Box.createRigidArea(new Dimension(20,0)));
+	    horizGroup.add(Box.createHorizontalGlue());
 
 	    userInput = new JTextField();
-	    dialogPanel.add(userInput);
+	    horizGroup.add(userInput);
+
+	    // Create glue/gap between username prompt
+	    //horizGroup.add(Box.createHorizontalGlue());
+	    horizGroup.add(Box.createRigidArea(new Dimension(50,0)));
+
+	    /* ********************************************************** */
+	    /*                 End of horizontal JPanel                   */
+	    /* ********************************************************** */
+
+	    dialogPanel.add(horizGroup);
+
+	    // Gap between JPanel and buttons
+	    dialogPanel.add(Box.createRigidArea(new Dimension(0,15)));
 	    
 	    JButton addButton = new JButton("Add EC");
 	    addButton.addActionListener(this);
@@ -189,9 +231,18 @@ class ManageECGUI implements ActionListener
 	    cancelButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 	    
 	    dialogPanel.add(addButton);
-	    dialogPanel.add(cancelButton);
+
+	    // Small gap between buttons
+	    dialogPanel.add(Box.createRigidArea(new Dimension(0,5)));
 	    
-	    //setSize(400,350);
+	    dialogPanel.add(cancelButton);
+
+	    // Small gap after buttons
+	    dialogPanel.add(Box.createRigidArea(new Dimension(0,10)));
+	    
+	    setSize(400,140);
+	    setResizable(false);
+	    
 	    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 	    getContentPane().add(dialogPanel);
 	    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
@@ -209,11 +260,11 @@ class ManageECGUI implements ActionListener
 		    
 		    if(newEC.equals(""))
 			{
-			    JOptionPane.showMessageDialog(null,"No Username was entered. Please enter in a Username","Incorrect Username",JOptionPane.ERROR_MESSAGE);
+			    JOptionPane.showMessageDialog(null,"No Username was entered\n\nPlease enter in a Username","Incorrect Username",JOptionPane.ERROR_MESSAGE);
 			}
 		    else if(holder.hasUser(newEC))
 			{
-			    JOptionPane.showMessageDialog(null,"The Username you entered is already an EC. Please enter in a Username that is not already an EC","Incorrect Username",JOptionPane.ERROR_MESSAGE);
+			    JOptionPane.showMessageDialog(null,"The Username you entered is already an EC\n\nPlease enter in a Username that is not already an EC","Incorrect Username",JOptionPane.ERROR_MESSAGE);
 			}
 		    else
 			{
@@ -224,17 +275,17 @@ class ManageECGUI implements ActionListener
 			    // alert that entered username is unable to become an EC.
 			    if(!temp.isUser())
 				{
-				    JOptionPane.showMessageDialog(null,"The Username entered is not a valid Username in the System. Please enter a valid Username","Incorrect Username",JOptionPane.ERROR_MESSAGE);
+				    JOptionPane.showMessageDialog(null,"The Username entered is not a valid Username in the System\n\nPlease enter a valid Username","Incorrect Username",JOptionPane.ERROR_MESSAGE);
 				}
 			    else if(!temp.isStudent())
 				{
-				    JOptionPane.showMessageDialog(null,"The Username entered is not a Student. Please enter a Username associated with a Student","Incorrect Username",JOptionPane.ERROR_MESSAGE);
+				    JOptionPane.showMessageDialog(null,"The Username entered is not a Student\n\nPlease enter a Username associated with a Student","Incorrect Username",JOptionPane.ERROR_MESSAGE);
 				}
 			    else
 				{
 				    holder.addEC(newEC);
 				    
-				    JOptionPane.showMessageDialog(null,"The Username was successfully added to the list of Election Commissioners","Success",JOptionPane.PLAIN_MESSAGE);
+				    JOptionPane.showMessageDialog(null,"Username has been added","Success",JOptionPane.PLAIN_MESSAGE);
 
 				    updateList();
 				    this.dispose();
